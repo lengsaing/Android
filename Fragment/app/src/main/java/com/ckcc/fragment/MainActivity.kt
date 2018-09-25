4e6adf1ffec8552dc02fafbc7a996f0119a0819b
Leng <lsaing@zamanuniversity.edu.kh>
2018-09-25 19:51:35 +0700

    no message

--- Fragment/app/src/main/java/com/ckcc/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/ckcc/fragment/MainActivity.kt
@@ -6,27 +6,39 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.ckcc.fragment.fragments.ExampleFragment
+import com.ckcc.fragment.fragments.ImageFragment
 import com.ckcc.fragment.fragments.ItemFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ExampleFragment.OnFragmentInteractionListener {
 
+    var arr = arrayListOf(ExampleFragment.newInstance("Leng", 19), ItemFragment(), ImageFragment())
+    var count = 0
+
+    override fun onButtonClick(){
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, arr.get(count))
+                .commit()
+        count++
+        if(count >= arr.size){
+            count = 0
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var arr = arrayListOf(ExampleFragment.newInstance("Leng", 19), ItemFragment())
-        var count = 0
+
 
         btn1.setOnClickListener {
             //send activity in fragment using newInstance
             supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, arr.get(count))
                     .commit()
-            count++
-            if(count >= arr.size){
-                count = 0
+            count--
+            if(count <= -1){
+                count = arr.lastIndex
             }
         }
 
@@ -40,9 +52,5 @@ class MainActivity : AppCompatActivity(), ExampleFragment.OnFragmentInteractionL
             }
         }
     }
-    override fun onButtonClick(){
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, ItemFragment())
-                .commit()
-    }
+
 }
--- Fragment/app/src/main/java/com/ckcc/fragment/fragments/ImageFragment.kt
+++ Fragment/app/src/main/java/com/ckcc/fragment/fragments/ImageFragment.kt
@@ -7,60 +7,58 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
 
 import com.ckcc.fragment.R
-import kotlinx.android.synthetic.main.fragment_example.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_NAME = "name"
-private const val ARG_AGE = "age"
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [ExampleFragment.OnFragmentInteractionListener] interface
+ * [ImageFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [ExampleFragment.newInstance] factory method to
+ * Use the [ImageFragment.newInstance] factory method to
  * create an instance of this fragment.
  *
  */
-class ExampleFragment : Fragment() {
+class ImageFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var name: String? = null
-    private var age: Int? = 0
+    private var param1: String? = null
+    private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            //attach the const to the variables for usage
-            name = it.getString(ARG_NAME)
-            age = it.getInt(ARG_AGE)
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        var view = inflater.inflate(R.layout.fragment_example, container, false)
-        var mybutton = view.findViewById<Button>(R.id.btnfragment)
-        mybutton.setOnClickListener {
-            listener?.onButtonClick()
-        }
-        Toast.makeText(context, "$name is $age", Toast.LENGTH_SHORT).show()
+        var view =  inflater.inflate(R.layout.fragment_image, container, false)
         return view
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        /*
         if (context is OnFragmentInteractionListener) {
             listener = context
         } else {
             throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
+        */
     }
 
     override fun onDetach() {
@@ -81,20 +79,25 @@ class ExampleFragment : Fragment() {
      */
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        fun onButtonClick()
+        fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
-
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ImageFragment.
+         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(name: String, age: Int) =
-                ExampleFragment().apply {
+        fun newInstance(param1: String, param2: String) =
+                ImageFragment().apply {
                     arguments = Bundle().apply {
-
-                        //put sent arguments inside the constants
-                        putString(ARG_NAME, name)
-                        putInt(ARG_AGE, age)
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
                     }
                 }
     }
